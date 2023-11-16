f708d7257a993671ddb530b6d3bce07989b3fa0d
M_thew <thew1302@gmail.com>
2023-11-16 15:36:28 -0500

    upload with firebase storage (#17)
    upgrade to version 1.0.8

--- packages/frontend/components/Project/MediaUpload.jsx
+++ packages/frontend/components/Project/MediaUpload.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import { Modal, Upload } from "antd";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from "../../utils/firestore";
 
 const getBase64 = (file) =>
   new Promise((resolve, reject) => {
@@ -9,88 +11,90 @@ const getBase64 = (file) =>
     reader.onload = () => resolve(reader.result);
     reader.onerror = (error) => reject(error);
   });
+
+const UploadButton = (
+  <div>
+    <PlusOutlined />
+    <div
+      style={{
+        marginTop: 8,
+      }}
+    >
+      Upload
+    </div>
+  </div>
+);
+
 const App = ({ files, onChange }) => {
   const [previewOpen, setPreviewOpen] = useState(false);
   const [previewImage, setPreviewImage] = useState("");
+  const [progresspercent, setProgresspercent] = useState(0);
+  const [imgUrl, setImgUrl] = useState("");
   const [previewTitle, setPreviewTitle] = useState("");
-  const [fileList, setFileList] = useState([
-    // {
-    //   uid: '-1',
-    //   name: 'image.png',
-    //   status: 'done',
-    //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    // },
-    // {
-    //   uid: '-2',
-    //   name: 'image.png',
-    //   status: 'done',
-    //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    // },
-    // {
-    //   uid: '-3',
-    //   name: 'image.png',
-    //   status: 'done',
-    //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    // },
-    // {
-    //   uid: '-4',
-    //   name: 'image.png',
-    //   status: 'done',
-    //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    // },
-    // {
-    //   uid: '-xxx',
-    //   percent: 50,
-    //   name: 'image.png',
-    //   status: 'uploading',
-    //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    // },
-  ]);
-  const handleCancel = () => setPreviewOpen(false);
-  const handlePreview = async (file) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
-    }
-    setPreviewImage(file.url || file.preview);
-    setPreviewOpen(true);
-    setPreviewTitle(
-      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
-    );
+  const [fileList, setFileList] = useState([]);
+
+  const handlers = {
+    cancel: () => setPreviewOpen(false),
+    preview: async (file) => {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj);
+      }
+      setPreviewImage(file.url || file.preview);
+      setPreviewOpen(true);
+      setPreviewTitle(
+        file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
+      );
+    },
+    change: ({ fileList: newFileList }) => {
+      setFileList(newFileList);
+      onChange(newFileList);
+      console.log("FILE-LIST =====> ", fileList);
+    },
+    submit: (e) => {
+      console.log("FILE-LIST =====> ", e);
+      handlers.change({ fileList: e.fileList });
+
+      const file = e.file;
+      if (!file) return;
+      const storageRef = ref(storage, `projects/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgresspercent(progress);
+        },
+        (error) => {
+          console.error(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImgUrl(downloadURL);
+          });
+        }
+      );
+    },
   };
-  const handleChange = ({ fileList: newFileList }) => {
-    setFileList(newFileList);
-    onChange(newFileList);
-  };
-  
-  const uploadButton = (
-    <div>
-      <PlusOutlined />
-      <div
-        style={{
-          marginTop: 8,
-        }}
-      >
-        Upload
-      </div>
-    </div>
-  );
   return (
     <>
       <Upload
         // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        action="/api/helpers/file"
+        // action="/api/helpers/file"
         listType="picture-card"
         fileList={fileList}
-        onPreview={handlePreview}
-        onChange={handleChange}
+        onPreview={handlers.preview}
+        onChange={handlers.submit}
       >
-        {fileList.length >= 8 ? null : uploadButton}
+        {fileList.length >= 8 ? null : UploadButton}
       </Upload>
       <Modal
         open={previewOpen}
         title={previewTitle}
         footer={null}
-        onCancel={handleCancel}
+        onCancel={handlers.cancel}
       >
         <img
           alt="example"
